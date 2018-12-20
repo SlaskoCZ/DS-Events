a15830e252c10ee0939b063d1bdfe58737e02170
Slasko <slasko@seznam.cz>
2018-12-20 22:12:15 +0100

    Base Event View Implemented

---
+++ src/assets/js/components/event-message.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="event-message" v-bind:class="{new: message.isNew}">
+        <img :src="message.image" class="image"/> 
+        <div class="info">
+            <div class="subject">{{ message.subject + " - " + message.name }}</div>
+            <p class="message">{{ message.message }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['message']
+}
+</script>
+
+<style>
+
+</style>
