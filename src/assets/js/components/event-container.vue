edaa539be0f0a1f78d7f7ae5afa7872aaa4c1921
Slasko <slasko@seznam.cz>
2018-12-21 13:19:02 +0100

    Split to multiple Pages, Added Icons, added paging for messages

--- src/assets/js/components/event-container.vue
+++ src/assets/js/components/event-container.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="event-container">
-        <h4>{{ text }}</h4>
-        <template v-for="message in messages">
+        <h4 v-bind:class="textClass">{{ text }}</h4>
+        <template v-for="message in view">
             <event-message v-bind:key="message.index" v-bind:message="message"/>
         </template>
+        <button v-if="showPages < maxPages" @click="showPages = showPages + 1">Načíst další</button>
+        <button v-if="showPages > 1" @click="showPages = showPages - 1">Zpět</button>
     </div>
 </template>
 
@@ -11,28 +13,54 @@
 import EventMessage from './event-message.vue';
 
 export default {
-    props: ['text'],
+    props: ['text', 'textClass', 'messages', 'pageLimit'],
     data(){
         return{
-            messages: getData()
+            showPages: 1,
+            maxPages: 0
         }
     },
     components:{
         EventMessage
+    },
+    computed:{
+        pages: function(){
+            var pages = [];
+            var index = 0;
+            var messages = this.messages;
+
+            if(this.pageLimit == undefined){
+                
+                pages.push({index, messages});
+                return pages;
+            }
+
+            for (index = 0; index < this.messages.length / this.pageLimit; index++) {
+                var start = index * (this.pageLimit);
+                messages = this.messages.slice(start, start + this.pageLimit)
+                pages.push({index, messages});
+                this.maxPages++;
+            }
+            return pages;
+        },
+        view: function(){
+            var result = [];
+            
+            for (let index = 0; index < this.showPages; index++) {
+                for (let index2 = 0; index2 < this.pages[index].messages.length; index2++) {
+                    result.push(this.pages[index].messages[index2]);
+                }
+            }
+
+            return result;
+        }
     }
 }
-
- function getData(url){
-    return [
-        { message: "this is message! This is Reaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaly long message", subject: "subject", name: "Test User", index: 0, isNew: true }, 
-        { message: "this is message!", subject: "subject",  name: "Test User", index: 1, isNew: true }, 
-        { message: "this is message!", subject: "subject",  name: "Test User", index: 2, isNew: true }, 
-        { message: "this is message!", subject: "subject",  name: "Test User with long name", index: 3, isNew: true }, 
-        { message: "this is message!", subject: "subject",  name: "Test User", index: 4, isNew: true }, 
-        { message: "this is message!", subject: "subject0", name: "Test User", index: 11, isNew: false },
-        { message: "this is message!", subject: "subject1", name: "Test User", index: 12, isNew: false },
-        { message: "this is message!", subject: "subject2", name: "Test User", index: 13, isNew: false },
-        { message: "this is message!", subject: "subject3", name: "Test User", index: 14, isNew: false }
-        ];
- }
 </script>
+
+// TODO Show only x items per page add load more
+// TODO Mark All as Read
+// TODO Make date to nice date conversion
+
+
+
